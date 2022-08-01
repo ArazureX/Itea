8e6404fb8098f52363f2b480241cdc1254ea9be9
Arazure <bobryk3000@gmail.com>
2022-08-01 21:39:26 +0300

    text

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,9 +1,8 @@
 package test;
 
-import homework10.Dota2Engine;
-
-public class test {
+public class Test {
     public static void main(String[] args) {
         System.out.print(Double.valueOf("22.2")+ "text" + "some text");
+        System.out.println("text");
     }
 }
