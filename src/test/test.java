7b41751423e88a3e62e5539f1f8a9c0a937798f7
Arazure <bobryk3000@gmail.com>
2022-08-01 21:18:39 +0300

    commit

--- src/test/test.java
+++ src/test/test.java
@@ -4,6 +4,7 @@ import homework10.Dota2Engine;
 
 public class test {
     public static void main(String[] args) {
-        System.out.print(Double.valueOf("22.2"));
+        System.out.print(Double.valueOf("22.2")+ "text");
+
     }
 }
